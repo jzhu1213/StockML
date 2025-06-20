e987561b88fa9c4fb97079ee738451ae20362fe7
jzhu1213 <135761768+jzhu1213@users.noreply.github.com>
2025-06-20 18:40:26 -0400

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.preprocessing import StandardScaler
 
 # Function to load and preprocess stock data
